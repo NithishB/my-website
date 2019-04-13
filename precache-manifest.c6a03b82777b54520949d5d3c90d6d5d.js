8df5527c4ac424f4c561781f2f29a02b6b4df1d3
NithishB <nithish.mdh@gmail.com>
2019-04-14 02:07:17 +0530

    Updates

--- precache-manifest.c6a03b82777b54520949d5d3c90d6d5d.js
+++ precache-manifest.c6a03b82777b54520949d5d3c90d6d5d.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "50ff84adf38ee65b03c7",
-    "url": "/my-website/static/css/main.e8b3bd75.chunk.css"
+    "revision": "20988ba3b97192879295",
+    "url": "/my-website/static/css/main.d13d9603.chunk.css"
   },
   {
-    "revision": "50ff84adf38ee65b03c7",
-    "url": "/my-website/static/js/main.1adb707a.chunk.js"
+    "revision": "20988ba3b97192879295",
+    "url": "/my-website/static/js/main.b5f42fa9.chunk.js"
   },
   {
     "revision": "37063c54184f9fd8244e",
     "url": "/my-website/static/js/runtime~main.99d9029d.js"
   },
   {
-    "revision": "54ea621e9bd6b50dfbef",
+    "revision": "7a3a768b4dc6b61b5b73",
     "url": "/my-website/static/css/2.bb54fe9b.chunk.css"
   },
   {
-    "revision": "54ea621e9bd6b50dfbef",
-    "url": "/my-website/static/js/2.a6bbc099.chunk.js"
+    "revision": "7a3a768b4dc6b61b5b73",
+    "url": "/my-website/static/js/2.89e4dd1e.chunk.js"
   },
   {
     "revision": "83b30fb82f990136f40605f5cf9a0b2d",
     "url": "/my-website/static/media/profile_pic.83b30fb8.jpg"
   },
   {
-    "revision": "53697693cf1e68d41803fdf91ecb39b3",
+    "revision": "01ed3466106b5c3ed9845fa21d4d842a",
     "url": "/my-website/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-website/precache-manifest.d0ae521b425664f9049b4254a0a389ab.js"
+  "/my-website/precache-manifest.c6a03b82777b54520949d5d3c90d6d5d.js"
 );
 
 workbox.clientsClaim();
