85bfe766ffd2ba30d0e5caa812581a87bbfa283f
NithishB <nithish.mdh@gmail.com>
2019-04-13 17:28:32 +0530

    Updates

---
+++ precache-manifest.d0ae521b425664f9049b4254a0a389ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "50ff84adf38ee65b03c7",
+    "url": "/my-website/static/css/main.e8b3bd75.chunk.css"
+  },
+  {
+    "revision": "50ff84adf38ee65b03c7",
+    "url": "/my-website/static/js/main.1adb707a.chunk.js"
+  },
+  {
+    "revision": "37063c54184f9fd8244e",
+    "url": "/my-website/static/js/runtime~main.99d9029d.js"
+  },
+  {
+    "revision": "54ea621e9bd6b50dfbef",
+    "url": "/my-website/static/css/2.bb54fe9b.chunk.css"
+  },
+  {
+    "revision": "54ea621e9bd6b50dfbef",
+    "url": "/my-website/static/js/2.a6bbc099.chunk.js"
+  },
+  {
+    "revision": "83b30fb82f990136f40605f5cf9a0b2d",
+    "url": "/my-website/static/media/profile_pic.83b30fb8.jpg"
+  },
+  {
+    "revision": "53697693cf1e68d41803fdf91ecb39b3",
+    "url": "/my-website/index.html"
+  }
+];
